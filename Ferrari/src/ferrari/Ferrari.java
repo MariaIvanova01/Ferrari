112fa0f5f88991454c64c3fee45113f757dbbb4a
MariaIvanova01 <mimi200801@abv.bg>
2022-07-29 16:01:19 +0300

    Using interface to achieve abstraction

---
+++ Ferrari/src/ferrari/Ferrari.java
@@ -0,0 +1,30 @@
+package ferrari;
+
+public class Ferrari implements Car {
+    private final String model = "488-Spider";
+    private String driverName;
+
+    public Ferrari(String driverName) {
+        this.driverName = driverName;
+    }
+
+    @Override
+    public String brakes() {
+        return "Brakes!";
+    }
+
+    @Override
+    public String gas() {
+        return "brum-brum-brum-brrrrr";
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s/%s/%s/%s",model,brakes(),gas(),driverName);
+    }
+
+}
